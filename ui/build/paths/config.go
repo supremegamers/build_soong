636dd32a26b8f38ff5d2e59e7645a1f838378a84
Silesh K Nair <sileshn@gmail.com>
2020-12-25 10:32:51 +0530

    Add python3.9 to allowed paths
    *fixes building changelog on more bleeding edge distros like arch/manjaro/sid
    
    Change-Id: Ie2c8d038a3b897db422f10d129378fb84a2716c1

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -113,6 +113,7 @@ var Configuration = map[string]PathConfig{
 	"python3.6": Allowed,
 	"python3.7": Allowed,
 	"python3.8": Allowed,
+	"python3.9": Allowed,
 	"rsync":   Allowed,
 	"sh":      Allowed,
 	"tr":      Allowed,
