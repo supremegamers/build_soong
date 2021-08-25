2e3d801c71040d34929a11a70f062b14786546ed
sairam1411 <majgaonkars3@gmail.com>
2021-08-25 22:45:45 +0530

    Revert "soong: Give priority to modules in exported namespaces for bootjars"
    This reverts commit ddf5458d148e20b325182f1ee7d80ab8aed86e2d.

--- android/module.go
+++ android/module.go
@@ -533,9 +533,6 @@ type commonProperties struct {
 
 	// set by ImageMutator
 	ImageVariation string `blueprint:"mutated"`
-
-	// Whether this module provides a boot jar
-	BootJarProvider bool `blueprint:"mutated"`
 }
 
 type hostAndDeviceProperties struct {
@@ -799,10 +796,6 @@ func (m *ModuleBase) String() string {
 	}
 	sb.WriteString("}")
 	return sb.String()
-}
-
-func (a *ModuleBase) BootJarProvider() bool {
-	return a.commonProperties.BootJarProvider
 }
 
 // BaseModuleName returns the name of the module as specified in the blueprints file.
